bab06cfb359a77fe874343520da5f99b8923470b
Rozthegray <rozthegrey@gmail.com>
2026-02-18 01:35:56 -0800

    Update CORS allowed origins for production

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["https://ledger-guard-frontend.vercel.app/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
